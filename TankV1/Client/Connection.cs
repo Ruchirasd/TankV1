2c93cdfccc38ed0686e0abe23c6794d4e28323db
Ruchira <srucheez@gmail.com>
2015-10-20 07:37:18 +0530

    Connection1

---
+++ TankV1/Client/Connection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TankV1.Client
+{
+    class Connection
+    {
+        public const string SERVER_IP = "";
+        public const int SERVER_PORT = 7000;
+        private BinaryWriter writer;
+
+        private TcpListener listner;
+        private NetworkStream serverStream; 
+
+        public void ReceiveData() {
+            listner = new TcpListener(IPAddress.Parse(SERVER_IP),SERVER_PORT);
+            listner.Start();
+
+            }
+
+     }
+}
+
